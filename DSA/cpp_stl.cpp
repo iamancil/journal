478d31a0993b8f3fb48f9c9b16038b89d2279853
iamancil <24bcs015@nith.ac.in>
2025-06-26 23:18:32 +0530

    Tried learning vectors

--- DSA/cpp_stl.cpp
+++ DSA/cpp_stl.cpp
@@ -1,16 +1,26 @@
 #include <bits/stdc++.h>
+#include <utility>
+#include <vector>
 using namespace std;
 
-int main(){
+void learnPair() {
 
-  pair<int,int> p1 = {13,14};
+  pair<int, int> p1 = {13, 14};
   cout << p1.first;
 
-  pair<int, pair<int,int>> p2 = {13,{14,15}};
+  pair<int, pair<int, int>> p2 = {13, {14, 15}};
   cout << p2.second.first;
 
-  pair<int,int> arr[] = {{13,14},{14,15},{16,17}};
+  pair<int, int> arr[] = {{13, 14}, {14, 15}, {16, 17}};
   cout << arr[2].first;
-
 }
 
+void expainVectors() {
+  vector<int> v;
+  v.push_back(5);
+  v.emplace_back(6);
+
+  vector<pair<int, int>> p;
+  p.push_back({8,9});
+  p.emplace_back(5,6);
+}
